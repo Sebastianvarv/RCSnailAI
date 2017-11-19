86d1c0d01eb6ab78221fa28bdd4251dc5e6ea7b3
Sebastian <sebastian.varv@gmail.com>
2017-11-19 16:32:19 +0200

    reading image, resizing and prepairing training images

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import cv2
 import pandas as pd
+import numpy as np
 
 cap = cv2.VideoCapture("Data/20171029-201639.h264.avi")
-labels = pd.read_csv("20171029-201639.h264.csv")
+labels = pd.read_csv("Data/20171029-201639.h264.csv")
+
+frame_counter = 0
+processed_frames = []
+IMAGE_SIZE = (64, 64, 3)
 
 
+training_images = []
 while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    frame_counter += 1
+    result, frame = cap.read()
+    if result and frame_counter % 12 == 0:
+        resized = cv2.resize(frame, IMAGE_SIZE[:2])
+        processed_frames.append(resized)
+        if len(processed_frames) >= 4:
+            # cv2.imshow('frame', resized)
 
-    # Our operations on the frame come here
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            stacked_image = np.concatenate(processed_frames, axis=2)
+            training_images.append(stacked_image)
+            processed_frames.pop()
 
-    # Display the resulting frame
-    resized = cv2.resize(frame, (64, 64))
-
-    cv2.imshow('frame', resized)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
+
+training_images = np.asarray(training_images)
+
+print(training_images.shape)
