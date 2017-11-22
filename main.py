8014c3beea6b537a438a483fe7dc058f370bf61b
Sebastian <sebastian.varv@gmail.com>
2017-11-22 14:08:50 +0200

    Removed bug where images are the same, classifier still produces constant output.

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import cv2
-import pandas as pd
 import numpy as np
-from keras.models import Model, Sequential
-from keras.layers import Input, Conv2D, Activation, Flatten, Dense, LSTM, Reshape, Permute, TimeDistributed, \
-    MaxPooling2D, GRU, Dropout
-from keras.optimizers import Adam
+import pandas as pd
+from keras.layers import Conv2D, Flatten, Dense, LSTM, TimeDistributed, \
+    MaxPooling2D
+from keras.models import Sequential
 
 IMAGE_SIZE = (64, 64, 3)
-
 
 def extract_training_data(filename, csv_filename, image_size=(64, 64, 3)):
     cap = cv2.VideoCapture(filename)
@@ -22,16 +20,17 @@ def extract_training_data(filename, csv_filename, image_size=(64, 64, 3)):
         frame_counter += 1
         result, frame = cap.read()
         if result and frame_counter % 12 == 0:
+            # cv2.imshow("img", frame)
             resized = cv2.resize(frame, image_size[:2])
             processed_frames.append(resized)
-            if len(processed_frames) >= 5:
+            if len(processed_frames) >= 4:
                 # cv2.imshow('frame', resized)
 
                 # stacked_image = np.concatenate(processed_frames, axis=2)
                 # training_labels.append(labels[frame_counter])
-                training_images.append(processed_frames)
+                training_images.append(processed_frames.copy())
                 training_label_ids.append(frame_counter)
-                processed_frames.pop()
+                processed_frames.pop(0)
 
         if cv2.waitKey(1) & 0xFF == ord('q') or not result:
             break
@@ -40,7 +39,7 @@ def extract_training_data(filename, csv_filename, image_size=(64, 64, 3)):
     cap.release()
     cv2.destroyAllWindows()
 
-    training_images = np.asarray(training_images)
+    training_images = np.array(training_images)
     training_labels = labels.loc[training_label_ids]
 
     assert training_images.shape[0] == training_labels.shape[0]
@@ -55,15 +54,25 @@ if __name__ == "__main__":
                               input_shape=(4, 64, 64, 3)))
     model.add(TimeDistributed(MaxPooling2D((2, 2))))
     model.add(TimeDistributed(Flatten()))
-    model.add(LSTM(32))
-    model.add(Dense(4, activation='softmax'))
+    model.add(LSTM(32, return_sequences=False))
+    model.add(Dense(4, activation='linear'))
 
-    model.compile(loss="mse", optimizer="adam", metrics=["accuracy"])
+    model.compile(loss="mse", optimizer="adam")
     model.summary()
 
-    # print(labels.head())
+    print(labels.head())
 
     y_train = labels.as_matrix(columns=labels.columns[1:])
+
+    prediction = model.predict(images)
+
+    for i in range(len(prediction)):
+        if i % 10 == 0:
+            print(np.mean(images[i], axis=(1, 2, 3)), prediction[i])
+
+    # for i in range(len(images)):
+    #     # print(np.mean(images[i], axis=(1,2,3)))
+    #     print(labels.head(10))
 
     history = model.fit(images, y_train, batch_size=64, epochs=5, validation_split=0.04)
 
@@ -71,4 +80,10 @@ if __name__ == "__main__":
 
     prediction = model.predict(images)
 
-    print(prediction)
+    for i in range(len(prediction)):
+        if i % 10 == 0:
+            print(np.mean(images[i], axis=(1,2,3)), prediction[i])
+
+    # #
+    # for i in range(len(images)):
+    #     print(np.mean(images[i], axis=(1,2,3)))
