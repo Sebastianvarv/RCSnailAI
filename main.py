ac86facc03561f95ca5d99cf4800e95a3afef2bf
Martin Liivak <martin.liivak@gmail.com>
2017-12-18 16:36:51 +0200

    Created separate main executable script to run the car using model.

--- main.py
+++ main.py
@@ -1,89 +1,51 @@
-import cv2
 import numpy as np
-import pandas as pd
-from keras.layers import Conv2D, Flatten, Dense, LSTM, TimeDistributed, \
-    MaxPooling2D
-from keras.models import Sequential
+from math import floor
+from car_interfacing import CarConnection
+from keras.models import load_model
 
-IMAGE_SIZE = (64, 64, 3)
+model = load_model('conv_dense_gear_bigdata.h5')
+connection = CarConnection()
 
-def extract_training_data(filename, csv_filename, image_size=(64, 64, 3)):
-    cap = cv2.VideoCapture(filename)
-    labels = pd.read_csv(csv_filename, sep="\t")
+frame_counter = 0
+processed_frames = []
 
-    frame_counter = 0
-    processed_frames = []
-
-    training_images = []
-    training_label_ids = []
-    while True:
+while True:
+    try:
         frame_counter += 1
-        result, frame = cap.read()
-        if result and frame_counter % 12 == 0:
-            # cv2.imshow("img", frame)
-            resized = cv2.resize(frame, image_size[:2])
-            processed_frames.append(resized)
-            if len(processed_frames) >= 4:
-                # cv2.imshow('frame', resized)
 
-                # stacked_image = np.concatenate(processed_frames, axis=2)
-                # training_labels.append(labels[frame_counter])
-                training_images.append(processed_frames.copy())
-                training_label_ids.append(frame_counter)
+        # connect to 22241 and retreive rescaled frame
+        frame = connection.receive_data_from_stream()
+
+        if frame is not None:
+            # Normalizing frame
+            normed = frame / 255
+            processed_frames.append(normed)
+
+            if len(processed_frames) >= 4:
+                stacked_image = np.concatenate(processed_frames, axis=2)
                 processed_frames.pop(0)
 
-        if cv2.waitKey(1) & 0xFF == ord('q') or not result:
+                prediction = model.predict(stacked_image[np.newaxis, :])
+                pred_list = prediction.tolist()[0]
+
+                # Disallow too sudden turns
+                pred_list[0] = pred_list[0] if abs(pred_list[0]) <= 0.6 else floor(pred_list[0] / 0.6) * 0.6
+
+                # Eliminate braking element if it's too small
+                pred_list[1] = pred_list[1] if pred_list[1] >= 0.4 else 0.0
+
+                # Normalize throttle to be always an effective input
+                pred_list[2] = 0.35 + np.clip(pred_list[2], 0, 1) * 0.4
+
+                # Round gear to closest integer
+                pred_list[3] = 2 if pred_list[3] >= 1.3 else 1
+
+                connection.send_commands_to_car(pred_list)
+
+        # Some breaking condition to kill me here
+        if frame_counter >= 15000:
             break
+    except KeyboardInterrupt:
+        connection.close()
 
-    # When everything done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
-
-    training_images = np.array(training_images)
-    training_labels = labels.loc[training_label_ids]
-
-    assert training_images.shape[0] == training_labels.shape[0]
-    return training_images, training_labels
-
-
-if __name__ == "__main__":
-    images, labels = extract_training_data("Data/20171029-201949.h264.avi", "Data/20171029-201949.h264.csv", IMAGE_SIZE)
-
-    model = Sequential()
-    model.add(TimeDistributed(Conv2D(32, (3, 3), kernel_initializer="he_normal", activation='relu'),
-                              input_shape=(4, 64, 64, 3)))
-    model.add(TimeDistributed(MaxPooling2D((2, 2))))
-    model.add(TimeDistributed(Flatten()))
-    model.add(LSTM(32, return_sequences=False))
-    model.add(Dense(4, activation='linear'))
-
-    model.compile(loss="mse", optimizer="adam")
-    model.summary()
-
-    print(labels.head())
-
-    y_train = labels.as_matrix(columns=labels.columns[1:])
-
-    prediction = model.predict(images)
-
-    for i in range(len(prediction)):
-        if i % 10 == 0:
-            print(np.mean(images[i], axis=(1, 2, 3)), prediction[i])
-
-    # for i in range(len(images)):
-    #     # print(np.mean(images[i], axis=(1,2,3)))
-    #     print(labels.head(10))
-
-    history = model.fit(images, y_train, batch_size=64, epochs=5, validation_split=0.04)
-
-    images, _ = extract_training_data("Data/20171029-201639.h264.avi", "Data/20171029-201639.h264.csv", IMAGE_SIZE)
-
-    prediction = model.predict(images)
-
-    for i in range(len(prediction)):
-        if i % 10 == 0:
-            print(np.mean(images[i], axis=(1,2,3)), prediction[i])
-
-    # #
-    # for i in range(len(images)):
-    #     print(np.mean(images[i], axis=(1,2,3)))
+connection.close()
