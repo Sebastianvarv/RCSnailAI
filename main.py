925123e6fe533c43e0e3054c9d3a253d1dfacea1
Martin Liivak <martin.liivak@gmail.com>
2017-12-18 17:25:23 +0200

    Slight changes.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import numpy as np
-from math import floor
 from car_interfacing import CarConnection
 from keras.models import load_model
 
@@ -13,7 +12,7 @@ while True:
     try:
         frame_counter += 1
 
-        # connect to 22241 and retreive rescaled frame
+        # Connect to car and retreive rescaled frame
         frame = connection.receive_data_from_stream()
 
         if frame is not None:
@@ -21,6 +20,7 @@ while True:
             normed = frame / 255
             processed_frames.append(normed)
 
+            # Stack frames and process
             if len(processed_frames) >= 4:
                 stacked_image = np.concatenate(processed_frames, axis=2)
                 processed_frames.pop(0)
@@ -28,24 +28,27 @@ while True:
                 prediction = model.predict(stacked_image[np.newaxis, :])
                 pred_list = prediction.tolist()[0]
 
-                # Disallow too sudden turns
-                pred_list[0] = pred_list[0] if abs(pred_list[0]) <= 0.6 else floor(pred_list[0] / 0.6) * 0.6
+                # Disallow too sudden turns (semi-bad idea)
+                # pred_list[0] = pred_list[0] if pred_list[0] >= -0.5 else (pred_list[0]/abs(pred_list[0])) * 0.5
 
                 # Eliminate braking element if it's too small
-                pred_list[1] = pred_list[1] if pred_list[1] >= 0.4 else 0.0
+                pred_list[1] = pred_list[1] if pred_list[1] >= 0.5 else 0.0
 
-                # Normalize throttle to be always an effective input
-                pred_list[2] = 0.35 + np.clip(pred_list[2], 0, 1) * 0.4
+                # Normalize throttle to be always an effective input (0.3 or so minimum)
+                pred_list[2] = 0.2 + np.clip(pred_list[2], 0, 1) * 0.7
 
                 # Round gear to closest integer
-                pred_list[3] = 2 if pred_list[3] >= 1.3 else 1
+                pred_list[3] = 2 if pred_list[3] >= 1.4 else 1
 
                 connection.send_commands_to_car(pred_list)
 
         # Some breaking condition to kill me here
         if frame_counter >= 15000:
             break
+
     except KeyboardInterrupt:
+        print('Connection killed.')
         connection.close()
 
+print('Connection killed.')
 connection.close()
