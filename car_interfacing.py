f5e2e54f8d91314d76022a7a929c6ecb06d8c368
Martin Liivak <martin.liivak@gmail.com>
2018-01-11 19:26:41 +0200

    Slight aesthetic refactor

--- car_interfacing.py
+++ car_interfacing.py
@@ -6,6 +6,7 @@ import numpy as np
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
+
 class CarConnection(object):
     def __init__(self, machine_name='tigu6'):
         remote_ip = socket.gethostbyname(machine_name)
@@ -15,9 +16,7 @@ class CarConnection(object):
         self.send_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.send_sock.connect(send_server)
 
-        # Temp removed until fixes are made
         self.recv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #self.recv_sock.settimeout(0.2)
         self.recv_sock.connect(receive_server)
 
     def send_commands_to_car(self, commands):
@@ -39,10 +38,6 @@ class CarConnection(object):
         self.send_sock.sendall((json_body + '\n').encode('utf-8'))
 
     def receive_data_from_stream(self):
-        # Temporarily open it every time it asks due to issues
-        #self.recv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #self.recv_sock.connect((socket.gethostbyname('tigu6'), 22242))
-
         header = self.recv_sock.recv(4)
         bytes_to_read = int.from_bytes(header, byteorder='little')
 
@@ -59,16 +54,11 @@ class CarConnection(object):
         img_array = np.asarray(img.convert('RGB'))
 
         img_array = np.flip(img_array, axis=2)
-        # Temporarily shut it down every time it asks due to issues
-        #self.recv_sock.shutdown(socket.SHUT_WR)
-        #self.recv_sock.close()
-
         return img_array
 
     def close(self):
         self.send_sock.shutdown(socket.SHUT_WR)
         self.send_sock.close()
 
-        # Temp removed because it's closed every time it opens
         self.recv_sock.shutdown(socket.SHUT_WR)
         self.recv_sock.close()
